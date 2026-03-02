08b0a02dad1b025defe1c02225bf31842af3c255
SevTify[404] <gametech228@gmail.com>
2026-03-02 07:26:56 +0000

    feat: Ajout du modèle de données pour les stories et des relations associées  - Création du modèle "Story" pour gérer les stories sur la plateforme, optimisé pour la pagination basée sur le curseur.  - Ajout de la relation entre les classes, clubs et utilisateurs avec les stories.  - Mise à jour des modèles existants pour intégrer les nouvelles relations de stories et ajustement des messages d'erreur.

---
+++ app/db/models/story_views.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime, Index, ForeignKey, func, PrimaryKeyConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+from app.db.models.mixins.integrity_error_mixin import IntegrityMapperMixin
+
+# Noms des contraintes
+FK_VIEWED_STORIES_USER = "fk_viewed_story_user"
+FK_VIEWED_STORIES_STORY = "fk_viewed_story_story"
+IDX_VIEWED_AT = "viewed_at_story_index"
+PK_STORY_VIEWS = "pk_story_views"
+
+
+class StoryViews(Base, IntegrityMapperMixin):
+    """Vu des stories"""
+
+    __tablename__ = "story_views"
+
+    # Attributs
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE", name=FK_VIEWED_STORIES_USER), nullable=False)
+    story_id: Mapped[UUID] = mapped_column(ForeignKey("stories.id", ondelete="CASCADE", name=FK_VIEWED_STORIES_STORY), nullable=False)
+
+    # Timestamps
+    viewed_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable=False, init=False)
+
+    # Index
+    __table_args__ = (
+        Index(IDX_VIEWED_AT, "viewed_at"),
+        PrimaryKeyConstraint("story_id", "user_id", name=PK_STORY_VIEWS)
+    )
+
+    # Relationships
+    user: Mapped["User"] = relationship("User", back_populates="viewed_stories", uselist=False, init=False)
+    story: Mapped["Story"] = relationship("Story", foreign_keys=[story_id], back_populates="views", uselist=False, init=False)
+
+    ERROR_MESSAGES = {
+        PK_STORY_VIEWS: "Un utilisateur ne peut voir une même story qu'une seule fois.",
+        FK_VIEWED_STORIES_STORY: "La story associée à une vue doit exister.",
+        FK_VIEWED_STORIES_USER: "L'utilisateur associé à une vue doit exister.",
+    }
