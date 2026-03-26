c5a95967eecd236bf672d3b636926832a0a40ba5
SevTify[404] <gametech228@gmail.com>
2026-03-26 04:04:36 +0000

    refactor: Petit renommage de deux trois trucs

--- app/schemas/global_schemas.py
+++ app/schemas/global_schemas.py
@@ -1,19 +1,23 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.schemas import ApiBaseResponse
 
 
 class StringMessage(BaseModel):
-  """Schémas pydantic pour valider le retour d'un message String"""
-  
-  message: str
+  """Schémas pydantic pour le retour d'un message de type String"""
+
+  message: str = Field(description="Le message de réponse relatif au résultat de l'opération, ce message là sera"
+                                   " forcément pour un succès, si c'est echec ca sera dans le champ 'error'")
 
 
 class GlobalStringMessage(ApiBaseResponse):
   
-  """Réponse contennant uniquement un message de type string, utiliser pour les endpoints qui ne retournent pas de données spécifiques mais juste un message de succès"""
+  """
+  Réponse contennant uniquement un message de type string, utiliser pour les endpoints qui ne retournent
+  pas de données spécifiques mais juste un message de succès
+  """
 
   result: Optional[StringMessage]
 
