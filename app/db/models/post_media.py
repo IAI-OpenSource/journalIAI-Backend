169cdb37d3123e381c10e4688b0efac50f02d043
SevTify[404] <gametech228@gmail.com>
2026-03-28 02:37:03 +0000

    feat: Ajout de contraintes d'unicité sur les rôles des utilisateurs et nouvelles colonnes dans la table des médias  - Mise en place de contraintes d'unicité pour les rôles exécutifs dans la table des utilisateurs afin d'éviter les doublons.  - Ajout de nouvelles colonnes 'blur_hash' et 'stored_bucket_name' dans la table 'post_media' pour enrichir les informations sur les médias.

--- app/db/models/post_media.py
+++ app/db/models/post_media.py
@@ -36,9 +36,9 @@ class PostMedia(Base, IntegrityMapperMixin):
 
     # Détails du média
     media_type: Mapped[MediaType] = mapped_column(SQLEnum(MediaType), nullable=False)
-    media_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    media_url: Mapped[str] = mapped_column(String(500), nullable=False)
     blur_hash: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
-    stored_bucket_name: Mapped[str] = mapped_column(String(500), nullable=False)
+    stored_bucket_name: Mapped[str] = mapped_column(String(255), nullable=False)
     thumbnail_url: Mapped[Optional[str]] = mapped_column(String(500), nullable=True)
 
     # Métadonnées
