072b760d36d038444206b1f500fb1521125c6f0b
SevTify[404] <gametech228@gmail.com>
2026-03-28 14:24:25 +0000

    fix: petits fix avant de merge

--- app/routers/club_router.py
+++ app/routers/club_router.py
@@ -11,10 +11,10 @@ from uuid import UUID
 
 router = APIRouter(prefix="/clubs", tags=[ApiTags.CLUB])
 
+#TODO: Ajouter une pagination cursor-based
 @router.get("/{club_id}", response_model=ApiBaseResponse[ClubResponse], status_code=200)
 async def get_club_by_id(club_id: UUID, reponse: Response , db: AsyncSession = Depends(get_db), redis = Depends(get_redis)) -> ApiBaseResponse[ClubResponse]:
-    """Endpoint pour récupérer un club par son ID.
-    """
+    """Endpoint pour récupérer un club par son ID."""
     club_service = ClubService(db, redis)
     result = await club_service.get_club(club_id)
     return result.to_HTTP_api_base_response(reponse)
@@ -28,19 +28,10 @@ async def get_club_by_slug(slug: str, reponse: Response , db: AsyncSession = Dep
     result = await club_service.get_club_by_slug(slug)
     return result.to_HTTP_api_base_response(reponse)
 
+#TODO: Mettre en cache cette les réponses de cette pagination dans le service
 @router.get("/", response_model=ApiBaseResponse[ClubsListResponse], status_code=200)
 async def get_all_clubs(reponse: Response , db: AsyncSession = Depends(get_db), redis = Depends(get_redis), page:int = 1, page_size: int = 20, is_active: bool = False, ) -> ApiBaseResponse[ClubsListResponse]:
-    """Endpoint pour lister tous les clubs.
-    
-    Args:
-        page (int): Le numéro de la page à récupérer.
-        page_size (int): Le nombre de clubs à récupérer par page.
-        is_active (bool): Filtrer les clubs par statut actif.
-        db (AsyncSession): La session de base de données, injectée par FastAPI.
-        
-    Returns:
-        ClubsListResponse: La liste des clubs.
-    """
+    """Endpoint pour lister tous les clubs avec pagination et filtrage par statut actif/inactif."""
     club_service = ClubService(db, redis)
     result = await club_service.get_all_clubs(page=page, page_size=page_size, is_active=is_active)
     return result.to_HTTP_api_base_response(reponse)
@@ -48,15 +39,7 @@ async def get_all_clubs(reponse: Response , db: AsyncSession = Depends(get_db), 
 
 @router.post("/", response_model=ApiBaseResponse[ClubResponse], status_code=201, )
 async def create_club(payload:ClubCreateRequest, reponse:Response, db: AsyncSession = Depends(get_db), redis = Depends(get_redis)) -> ApiBaseResponse[ClubResponse]:
-    """Endpoint pour créer un club.
-    
-    Args:
-        payload (ClubCreateRequest): Les données du club à créer.
-        db (AsyncSession): La session de base de données, injectée par FastAPI.
-        
-    Returns:
-        ClubResponse: Les données du club créé ou une erreur si la création échoue.
-    """
+    """Endpoint pour créer un club"""
     club_service = ClubService(db, redis)
     result = await club_service.create_club(payload)
     return result.to_HTTP_api_base_response(reponse)
@@ -64,31 +47,14 @@ async def create_club(payload:ClubCreateRequest, reponse:Response, db: AsyncSess
 
 @router.put("/{club_id}", response_model=ApiBaseResponse[ClubResponse], status_code=200)
 async def update_club(club_id: UUID, payload: ClubUpdateRequest, reponse: Response, db: AsyncSession = Depends(get_db), redis = Depends(get_redis)) -> ApiBaseResponse[ClubResponse]:
-    """Endpoint pour mettre à jour un club.
-    
-    Args:
-        club_id (UUID): L'identifiant du club à mettre à jour.
-        payload (ClubUpdateRequest): Les données du club à mettre à jour.
-        db (AsyncSession): La session de base de données, injectée par FastAPI.
-        
-    Returns:
-        ClubResponse: Les données du club mis à jour ou une erreur si la mise à jour échoue.
-    """
+    """Endpoint pour mettre à jour un club"""
     club_service = ClubService(db, redis)
     result = await club_service.update_club(club_id, payload)
     return result.to_HTTP_api_base_response(reponse)
 
 @router.delete("/{club_id}", response_model=GlobalStringMessage, status_code=200)
 async def delete_club(club_id: UUID, reponse: Response, db: AsyncSession = Depends(get_db), redis = Depends(get_redis)) -> ApiBaseResponse[None]:
-    """Endpoint pour supprimer un club.
-    
-    Args:
-        club_id (UUID): L'identifiant du club à supprimer.
-        db (AsyncSession): La session de base de données, injectée par FastAPI.
-        
-    Returns:
-        None: Une réponse vide avec un code de statut indiquant le résultat de l'opération.
-    """
+    """Endpoint pour supprimer un club"""
     club_service = ClubService(db, redis)
     result = await club_service.delete_club(club_id)
     return result.to_HTTP_api_base_response(reponse)
--- app/services/registration_service.py
+++ app/services/registration_service.py
@@ -12,6 +12,7 @@ from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.models.enums import UserRole
 from app.repositories.registration_repository import RegistrationRepository
 from app.schemas.global_schemas import GlobalStringMessage, StringMessage
 from app.schemas.registration_schemas import CreateMultileRegistration, CreateRegistration, FindRegistration, ReadRegistration
@@ -66,13 +67,13 @@ class RegistrationService:
       )
       
     return ServiceResult.service_success(
-      data=ReadRegistration(**repo_reg.data),
+      data=ReadRegistration.model_validate(repo_reg.data),
       status_code=repo_reg.status_code,
       service_name=msg.READ_REGISTRATION
     )
     
     
-  async def service_imports_reg_data(self, file_base: str, classe_id: UUID) -> ServiceResult[str]:
+  async def service_imports_reg_data(self, file_base: str, classe_id: UUID) -> ServiceResult[StringMessage]:
     """Logique métier pour générer plusieurs jeton en meme temps
       (à partir d'un fichier excel)"""
 
@@ -85,6 +86,7 @@ class RegistrationService:
 
     for row in valide_data:
       row["classe_id"] = classe_id
+      row["role"] = UserRole.STUDENT.value
       row["jeton"] = JetonUtils.generate_code_jeton(8)
         
     if valide_data:
@@ -92,7 +94,7 @@ class RegistrationService:
 
       if result.is_success():
         return ServiceResult.service_success(
-          data=StringMessage(result.data),
+          data=StringMessage(message=result.data),
           status_code=result.status_code,
           service_name=msg.REGISTRATION_JETON
         )
