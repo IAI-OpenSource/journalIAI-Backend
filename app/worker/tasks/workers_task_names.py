8898e4eae1d5b3dd45f3bc9fbec808c24753a1d4
SamMasHack207 <adjeteprosper@gmail.com>
2026-03-28 13:00:09 +0000

    ajout de process media tags

--- app/worker/tasks/workers_task_names.py
+++ app/worker/tasks/workers_task_names.py
@@ -1,11 +1,10 @@
-from dataclasses import dataclass
+from enum import StrEnum
 
 
-@dataclass
-class WorkersTaskNames:
+class WorkersTaskNames(StrEnum):
     """
     Classe pour centraliser les noms des tâches Celery utilisées dans l'application pour éviter les erreurs de frappe
     et faciliter la maintenance. Chaque nom de tâche est défini comme une constante de classe.
     """
-
-    SAVE_AUDIT_LOG: str = "audit_log.create"
+    SAVE_AUDIT_LOG = "save_audit_log"
+    PROCESS_MEDIA  = "process_media"
