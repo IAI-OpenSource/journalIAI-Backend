6bc468215360eb39c33ead9d14e93fcc62b17ed9
IsaacDMD <isaacdjimadjo830@gmail.com>
2026-03-29 18:26:25 +0000

    Début del'implementation de classe et academicYear

---
+++ app/schemas/academicYear_schemas.py
@@ -0,0 +1,80 @@
+# Ce fichier contient les différents schémas concernant les opérations
+# sur la table academic_year
+
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field
+from uuid import UUID
+from app.schemas import ApiBaseResponse
+
+class AcademicYearCreateRequest(BaseModel):
+    """ Schemas pydantic pour la création d'une année académique
+    
+    attributes:
+        libelle: Le libellé de l'année académique (ex: "2025-2026")
+        start_date: La date de début de l'année académique
+        end_date: La date de fin de l'année académique
+    """
+    
+    libelle: str = Field(..., min_length=5, max_length=20, description="Le libellé de l'année académique (ex: '2025-2026')")
+    start_date: datetime = Field(..., description="La date de début de l'année académique")
+    end_date: datetime = Field(..., description="La date de fin de l'année académique")
+
+class AcademicYearUpdateRequest(BaseModel):
+    """ Schemas pydantic pour la mise à jour d'une année académique
+    
+    attributes (optionnels):
+        libelle: Le libellé de l'année académique (ex: "2025-2026")
+        start_date: La date de début de l'année académique
+        end_date: La date de fin de l'année académique
+    """
+    
+    libelle: Optional[str] = Field(None, min_length=5, max_length=20, description="Le libellé de l'année académique (ex: '2025-2026')")
+    start_date: Optional[datetime] = Field(None, description="La date de début de l'année académique")
+    end_date: Optional[datetime] = Field(None, description="La date de fin de l'année académique")
+
+class AcademicYearResponse(BaseModel):
+    """ Schemas pydantic pour la réponse d'une année académique
+    
+    attributes:
+        id: L'identifiant de l'année académique
+        libelle: Le libellé de l'année académique (ex: "2025-2026")
+        start_date: La date de début de l'année académique
+        end_date: La date de fin de l'année académique
+        active: (bool) l'année académique est active ou pas
+        created_at: La date de création de l'année académique
+        updated_at: La date de mise à jour de l'année académique
+    """
+    
+    model_config = {"from_attributes": True}
+    id: UUID
+    libelle: str
+    start_date: datetime
+    end_date: datetime
+    active: bool
+    created_at: datetime
+    updated_at: datetime
+
+
+class AcademicYearListResponse(BaseModel):
+    """ Schemas pydantic pour la réponse d'une liste d'années académiques
+    
+    attributes:
+        classes: La liste des années académiques
+    """
+    
+    classes: list[AcademicYearResponse]
+    total: int
+    page : int
+    page_size : int
+
+
+class AcademicYearApiResponse(ApiBaseResponse[AcademicYearResponse]):
+    """ Schemas pydantic pour la réponse d'une année académique dans une API, hérite de ApiBaseResponse avec un result de type AcademicYearResponse"""
+    
+    pass
+
+class AcademicYearsListApiResponse(ApiBaseResponse[AcademicYearListResponse]):
+    """ Schemas pydantic pour la réponse d'une liste d'années académiques dans une API, hérite de ApiBaseResponse avec un result de type AcademicYearListResponse"""
+    
+    pass
--- app/schemas/classe_schemas.py
+++ app/schemas/classe_schemas.py
@@ -5,11 +5,11 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel,Field
 
 from uuid import UUID
 
-from app.db.models.enums import ClasseType, ExecutiveRoleType, SexeType, UserRole
+from app.db.models.enums import ClasseType
 from app.schemas import ApiBaseResponse
 
 
@@ -28,3 +28,74 @@ class ReadUserClasse(BaseModel):
   class Config:
     from_attributes=True
 
+
+class ClasseCreateRequest(BaseModel):
+  """ Schemas pydantic pour la création d'une classe
+
+  attributes:
+    classe_prefix: Le préfixe de la classe (ex: "TC1", "TC2", etc.)
+    classe_suffix: Le suffixe de la classe (ex: "A", "B", etc.)
+    academic_year_id: L'identifiant de l'année académique à laquelle appartient la classe
+  """
+
+  classe_prefix: ClasseType = Field(..., description="Le préfixe de la classe (ex: 'TC1', 'TC2', etc.)")
+  classe_suffix: str = Field(...,max_length=1, description="Le suffixe de la classe (ex: 'A', 'B', etc.)")
+  academic_year_id: UUID = Field(..., description="L'identifiant de l'année académique à laquelle appartient la classe")
+
+
+
+class ClasseUpdateRequest(BaseModel):
+  """ Schemas pydantic pour la mise à jour d'une classe
+
+  attributes:
+    classe_prefix: Le préfixe de la classe (ex: "TC1", "TC2", etc.)
+    classe_suffix: Le suffixe de la classe (ex: "A", "B", etc.)
+  """
+
+  classe_prefix: Optional[ClasseType] = Field(None, description="Le préfixe de la classe (ex: 'TC1', 'TC2', etc.)")
+  classe_suffix: Optional[str] = Field(None, max_length=1, description="Le suffixe de la classe (ex: 'A', 'B', etc.)")
+
+
+class ClasseResponse(BaseModel):
+  """ Schemas pydantic pour la réponse d'une classe
+
+  attributes:
+    id: L'identifiant de la classe
+    classe_prefix: Le préfixe de la classe (ex: "TC1", "TC2", etc.)
+    classe_suffix: Le suffixe de la classe (ex: "A", "B", etc.)
+    effectif: L'effectif de la classe
+    academic_year_id: L'identifiant de l'année académique à laquelle appartient la classe
+    created_at: La date de création de la classe
+    updated_at: La date de mise à jour de la classe
+  """
+
+  model_config = {"from_attributes": True}
+  id: UUID 
+  classe_prefix: ClasseType 
+  classe_suffix: str 
+  effectif: int 
+  academic_year_id: UUID 
+  created_at: datetime 
+  updated_at: datetime
+
+class ClasseListResponse(BaseModel):
+  """ Schemas pydantic pour la réponse d'une liste de classes
+
+    attributes:
+      classes: La liste des classes
+  """
+
+  classes: list[ClasseResponse]
+  total: int
+  page : int
+  page_size : int
+
+class ClasseApiResponse(ApiBaseResponse[ClasseResponse]):
+  """ Schemas pydantic pour la réponse d'une classe dans une API, hérite de ApiBaseResponse avec un result de type ClasseResponse"""
+
+  pass
+
+class ClassesListApiResponse(ApiBaseResponse[ClasseListResponse]):
+  """ Schemas pydantic pour la réponse d'une liste de classes dans une API, hérite de ApiBaseResponse avec un result de type ClasseListResponse"""
+
+  pass
